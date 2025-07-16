bf067b9638309c091018bd3e7e258581ebf9487f
Lívia Raissinger <liviaraissinger@gmail.com>
2025-07-16 19:20:13 -0300

    parte metasestrategicas

---
+++ src/main/java/org/serratec/backend/gestao_competencias/enums/StatusMeta.java
@@ -0,0 +1,9 @@
+package org.serratec.backend.gestao_competencias.enums;
+
+public enum StatusMeta {
+
+	PLANEJAMENTO,
+	ANDAMENTO,
+	CONCLUIDA,
+	CANCELADA
+}
---
+++ src/main/java/org/serratec/backend/gestao_competencias/repository/MetasEstrategicasRepository.java
@@ -0,0 +1,7 @@
+package org.serratec.backend.gestao_competencias.repository;
+
+import org.serratec.backend.gestao_competencias.entity.MetasEstrategicas;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MetasEstrategicasRepository extends JpaRepository<MetasEstrategicas, Long> {
+}
